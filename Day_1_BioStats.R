9dd68d8767e35711fb73c1957c7fe657a7d75ea1
MatthewArendse <3440197@myuwc.ac.za>
2018-04-12 13:12:14 +0200

    New Edits

--- Day_1_BioStats.R
+++ Day_1_BioStats.R
@@ -1,1 +1,9 @@
-#Day_1_Intro to GitHub
+#Day_1_Intro to GitHub_&_BioStats
+#12/04/2018
+
+#Matthew_Arendse
+#3440197
+
+#This serves as the starting point for the BSc BCB(Hons) BioStats Module 2018
+
+
